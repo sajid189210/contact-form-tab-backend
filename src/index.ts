b7a1724ab56fdb22043a026efa1acb12b89dea57
Muhammed Sajid S <sajid189210@gmail.com>
2026-02-18 12:18:43 +0530

    took port from env

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import app from './app'
 import connectDB from './core/configs/connect-db.config';
 
+const port = process.env.PORT || 8000;
 const start = async () => {
     await connectDB()
-    app.listen(3000, () => {
+    app.listen(port, () => {
         console.log('Server is running on port 3000');
     });
 }
